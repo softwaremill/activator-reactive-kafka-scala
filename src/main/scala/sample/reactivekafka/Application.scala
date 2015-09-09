d5e31176784b7a3b38dd18fb61bc541d9c13a7a2
Krzysztof Ciesielski <krzysztof.ciesielski@softwaremill.com>
2015-09-09 16:12:12 +0200

    [minor] Clean imports

--- src/main/scala/sample/reactivekafka/Application.scala
+++ src/main/scala/sample/reactivekafka/Application.scala
@@ -1,16 +1,6 @@
 package sample.reactivekafka
 
-import java.util.{ Properties, UUID }
-
-import akka.actor.{ ActorSystem, Props }
-import akka.stream.ActorMaterializer
-import akka.stream.actor.ActorPublisher
-import akka.stream.actor.ActorPublisherMessage.Cancel
-import akka.stream.scaladsl.{ Sink, Source }
-import com.softwaremill.react.kafka.{ ConsumerProperties, ProducerProperties, ReactiveKafka }
-import utils.embeddedkafka.KafkaLocal
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
+import akka.actor.{ActorSystem, Props}
 
 object Application extends App {
 
